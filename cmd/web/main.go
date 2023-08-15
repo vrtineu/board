43ba6dfd248bdbb32a98090d35a7251d0b318a3b
vrtineu <vininro@gmail.com>
2023-08-14 21:18:55 -0300

    feat: creating database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	host := os.Getenv("HOST")
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	dbname := os.Getenv("DBNAME")
+	port := os.Getenv("PORT")
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN: "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable TimeZone=America/NewYork",
+	}), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate() // TODO: add models
 }
